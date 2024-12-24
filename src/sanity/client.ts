f832ea684eb83540305e86babe5818807b8b0bd3
Matt Czencz <Mattczencz24@gmail.com>
2024-12-24 14:41:41 -0700

    Live preview work

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,6 +3,6 @@ import { createClient } from 'next-sanity';
 export const client = createClient({
   projectId: 'k88wbyzk',
   dataset: 'production',
-  apiVersion: '2024-11-01',
-  useCdn: false,
+  apiVersion: 'vX',
+  useCdn: true,
 });
--- src/sanity/live.ts
+++ src/sanity/live.ts
@@ -2,5 +2,5 @@ import { defineLive } from 'next-sanity';
 import { client } from '@/sanity/client';
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client: client.withConfig({ apiVersion: 'vX' }),
+  client,
 });
