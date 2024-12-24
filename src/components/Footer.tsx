7e8b68ff26aa608b82951e50a9add65f4a031917
Matt Czencz <Mattczencz24@gmail.com>
2024-12-24 15:06:15 -0700

    Updates footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row gap-4 items-center lg:gap-8 max-w-6xl w-full text-white">
         <FiGithub size={24} />
         <Link
-          href="https://github.com/mattczencz/portfolio-with-cms"
+          href="https://github.com/mattczencz/portfolio"
           target="_blank"
           className="text-xl text-center md:text-left text-white relative after:w-0 hover:after:w-4/5 after:border-b-2 after:left-0 after:-bottom-1 after:border-white after:block after:transition-all after:duration-200"
         >
