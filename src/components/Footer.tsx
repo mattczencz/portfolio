c7cebf191440cf480db5e999f5e1b7343d55fdc9
Matt Czencz <Mattczencz24@gmail.com>
2024-12-24 14:09:38 -0700

    Updates portfolio link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,8 @@ const Footer = () => {
     <section className="bg-theme-heading-text flex justify-center px-4 pb-32 pt-8 lg:py-8">
       <div className="flex flex-col md:flex-row gap-4 items-center lg:gap-8 max-w-6xl w-full text-white">
         <FiGithub size={24} />
-        {/* TODO: Replace link with new repo */}
         <Link
-          href="https://github.com/mattczencz/nextjs-portfolio"
+          href="https://github.com/mattczencz/portfolio-with-cms"
           target="_blank"
           className="text-xl text-center md:text-left text-white relative after:w-0 hover:after:w-4/5 after:border-b-2 after:left-0 after:-bottom-1 after:border-white after:block after:transition-all after:duration-200"
         >
